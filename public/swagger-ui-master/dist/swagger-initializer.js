4af6f6eb38633d2ab33ae4277c72d6061ba79c7b
wildanar29 <wildanar29@gmail.com>
2025-06-16 08:46:39 +0700

    swagger

--- public/swagger-ui-master/dist/swagger-initializer.js
+++ public/swagger-ui-master/dist/swagger-initializer.js
@@ -3,7 +3,8 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://app.rsimmanuel.net:9091/swagger/swagger.json",
+    // url: "https://app.rsimmanuel.net:9091/swagger/swagger.json",
+    url: "https://app.rsimmanuel.net:9091/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
