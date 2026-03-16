2c08b3e5b0876c67811583f248afea790a680b32
wildanar29 <wildanar29@gmail.com>
2026-03-16 12:47:47 +0700

    ada

--- resources/views/transkrip/nilai.blade.php
+++ resources/views/transkrip/nilai.blade.php
@@ -14,162 +14,151 @@ $logoBase64 = file_exists($logoPath)
 <style>
 
 @page {
-    size: A4 landscape;
-    margin: 0.8cm;
+size: A4 landscape;
+margin: 0.8cm;
 }
 
 body {
-    font-family: 'Times New Roman', serif;
-    font-size: 10px;
-    line-height: 1.25;
-    background-color: #fbfaf5;
-
-    width: 29.7cm;
-    height: 21cm;
-
-    margin: 0;
-    position: relative;
-    box-sizing: border-box;
-    border: 2px solid #c5a880;
-    padding: 1.2% 2%;
+font-family: 'Times New Roman', serif;
+font-size: 10px;
+line-height: 1.25;
+background-color: #fbfaf5;
+margin: 0;
+position: relative;
+box-sizing: border-box;
+border: 2px solid #c5a880;
+min-height: 98vh;
+padding: 1.2% 2%;
 }
 
-/* WATERMARK */
 body::after {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 45%;
-    height: 45%;
-    background: url('{{ $logoBase64 }}') no-repeat center;
-    background-size: contain;
-    opacity: 0.06;
-    transform: translate(-50%, -50%);
-    z-index: 0;
+content: "";
+position: absolute;
+top: 50%;
+left: 50%;
+width: 45%;
+height: 45%;
+background: url('{{ $logoBase64 }}') no-repeat center;
+background-size: contain;
+opacity: 0.06;
+transform: translate(-50%, -50%);
+z-index: 0;
 }
 
-/* BORDER DALAM */
 body::before {
-    content: "";
-    position: absolute;
-    top: 0.7%;
-    left: 0.7%;
-    right: 0.7%;
-    bottom: 0.7%;
-    border: 2px solid #c5a880;
-    z-index: 2;
+content: "";
+position: absolute;
+top: 0.7%;
+left: 0.7%;
+right: 0.7%;
+bottom: 0.7%;
+border: 2px solid #c5a880;
+z-index: 2;
 }
 
-/* LOGO */
 .logo {
-    position: absolute;
-    top: 15px;
-    left: 18px;
-    width: 60px;
-    z-index: 2;
+position: absolute;
+top: 15px;
+left: 18px;
+width: 60px;
+z-index: 2;
 }
 
-/* NOMOR SURAT */
 .nomor-surat {
-    position: absolute;
-    top: 18px;
-    right: 18px;
-    font-size: 10px;
-    font-weight: bold;
-    z-index: 2;
+position: absolute;
+top: 18px;
+right: 18px;
+font-size: 10px;
+font-weight: bold;
+z-index: 2;
 }
 
-/* HEADER */
 .main-content {
-    text-align: center;
-    margin-top: 40px;
+text-align: center;
+margin-top: 40px;
 }
 
 .main-content h2 {
-    margin-bottom: 4px;
-    font-size: 14px;
+margin-bottom: 4px;
+font-size: 14px;
 }
 
 .main-content p {
-    margin: 2px 0;
+margin: 2px 0;
 }
 
-/* TABLE */
 table.data-table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 10px;
+width: 100%;
+border-collapse: collapse;
+margin-top: 10px;
 }
 
 table.data-table th,
 table.data-table td {
-    border: 1px solid #000;
-    padding: 3px 5px;
+border: 1px solid #000;
+padding: 3px 5px;
 }
 
 table.data-table th {
-    background-color: #f0f0f0;
-    text-align: center;
-    font-size: 10px;
+background-color: #f0f0f0;
+text-align: center;
+font-size: 10px;
 }
 
 table.data-table td {
-    font-size: 10px;
+font-size: 10px;
 }
 
-/* FOOTER */
 .footer {
-    margin-top: 10px;
-    text-align: right;
-    font-size: 10px;
+margin-top: 10px;
+text-align: right;
+font-size: 10px;
 }
 
-/* SIGNATURE */
 .signature-wrapper {
-    width: 100%;
-    margin-top: 30px;
-    text-align: center;
+width: 100%;
+margin-top: 30px;
+text-align: center;
 }
 
 .signature-column {
-    width: 40%;
-    display: inline-block;
-    vertical-align: top;
+width: 40%;
+display: inline-block;
+vertical-align: top;
 }
 
 .signature-left {
-    text-align: left;
-    margin-right: 5%;
+text-align: left;
+margin-right: 5%;
 }
 
 .signature-right {
-    text-align: right;
-    margin-left: 5%;
+text-align: right;
+margin-left: 5%;
 }
 
 .signature-title {
-    font-size: 10px;
-    font-weight: bold;
-    margin-bottom: 4px;
+font-size: 10px;
+font-weight: bold;
+margin-bottom: 4px;
 }
 
 .signature-name {
-    margin-top: 6px;
-    font-weight: bold;
-    border-top: 1px solid #000;
-    display: inline-block;
-    padding-top: 3px;
-    font-size: 10px;
+margin-top: 6px;
+font-weight: bold;
+border-top: 1px solid #000;
+display: inline-block;
+padding-top: 3px;
+font-size: 10px;
 }
 
 .signature-reg {
-    font-size: 10px;
-    margin-top: 2px;
+font-size: 10px;
+margin-top: 2px;
 }
 
 .qr-img {
-    margin-top: 2px;
+margin-top: 2px;
 }
 
 </style>
@@ -194,6 +183,7 @@ No: {{ $nomor }}
 <p><strong>Level:</strong> {{ $gelar }}</p>
 
 </div>
+
 
 <table class="data-table">
 
@@ -222,9 +212,11 @@ No: {{ $nomor }}
 
 </table>
 
+
 <div class="footer">
 <p>{{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}</p>
 </div>
+
 
 <div class="signature-wrapper">
 
@@ -250,6 +242,7 @@ NIP: {{ $bidang_reg ?? '-' }}
 </div>
 
 </div>
+
 
 <!-- ASESOR -->
 <div class="signature-column signature-right">
